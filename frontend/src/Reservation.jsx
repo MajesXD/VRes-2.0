b3f53782dbef0eb4f6d38f3d46b9f5d25f4a8af7
MajesXD <majesxddd@o2.pl>
2026-03-04 17:26:44 +0100

    git wymiary

--- frontend/src/Reservation.jsx
+++ frontend/src/Reservation.jsx
@@ -1,9 +1,15 @@
 import './styles/Reservation.css'
 import {colors} from "./assets/colors";
+import Person from './assets/Person';
+import Clock from './assets/Clock';
+import Web from './assets/Web';
+import Phone from './assets/Phone';
+import WalkIn from './assets/WalkIn';
 const Reservation = ({people = '', time = ''}) => (
-    <div className={`reservation people${people} time${time}`}>
-        
-    </div>
+    <section className={`reservation people${people} time${time}`}>
+        <div className='reservation_row'><Person height={18} color='black'/><p>1</p><Clock height={18} color='black'/><p>2h</p></div>
+        <div className='reservation_row'><Web height={18} color='black'/><p></p></div>
+    </section>
 
 )
 export default Reservation;
