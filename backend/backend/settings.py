d4aa50e3420a3eec25a03e84acc90826079330c4
MajesXD <majesxddd@o2.pl>
2026-04-04 16:43:42 +0200

    bazoonia start

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,8 +74,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vres',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 60, 
     }
 }
 
